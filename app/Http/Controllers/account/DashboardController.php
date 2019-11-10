10a06989a9b7c9c59a9ef99c50c0d5fde7386edf
maulayyacyber <ridaulmaulayya@gmail.com>
2019-11-10 08:54:54 +0700

    ADD : info saldo on dashboard

--- app/Http/Controllers/account/DashboardController.php
+++ app/Http/Controllers/account/DashboardController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\account;
 
+use App\Debit;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -20,7 +24,56 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('account.dashboard.index');
+
+        $uang_masuk_bulan_ini  = DB::table('debit')
+            ->selectRaw('sum(nominal) as nominal')
+            ->whereYear('debit_date', Carbon::now()->year)
+            ->whereMonth('debit_date', Carbon::now()->month)
+            ->where('user_id', Auth::user()->id)
+            ->first();
+
+        $uang_keluar_bulan_ini = DB::table('credit')
+            ->selectRaw('sum(nominal) as nominal')
+            ->whereYear('credit_date', Carbon::now()->year)
+            ->whereMonth('credit_date', Carbon::now()->month)
+            ->where('user_id', Auth::user()->id)
+            ->first();
+
+        $uang_masuk_bulan_lalu  = DB::table('debit')
+            ->selectRaw('sum(nominal) as nominal')
+            ->whereYear('debit_date', Carbon::now()->year)
+            ->whereMonth('debit_date', Carbon::now()->subMonths())
+            ->where('user_id', Auth::user()->id)
+            ->first();
+
+        $uang_keluar_bulan_lalu = DB::table('credit')
+            ->selectRaw('sum(nominal) as nominal')
+            ->whereYear('credit_date', Carbon::now()->year)
+            ->whereMonth('credit_date', Carbon::now()->subMonths())
+            ->where('user_id', Auth::user()->id)
+            ->first();
+
+        $uang_masuk_selama_ini  = DB::table('debit')
+            ->selectRaw('sum(nominal) as nominal')
+            ->where('user_id', Auth::user()->id)
+            ->first();
+
+        $uang_keluar_selama_ini = DB::table('credit')
+            ->selectRaw('sum(nominal) as nominal')
+            ->where('user_id', Auth::user()->id)
+            ->first();
+
+
+        //saldo bulan ini
+        $saldo_bulan_ini = $uang_masuk_bulan_ini->nominal - $uang_keluar_bulan_ini->nominal;
+
+        //saldo bulan lalu
+        $saldo_bulan_lalu     = $uang_masuk_bulan_lalu->nominal - $uang_keluar_bulan_lalu->nominal;
+
+        //saldo selama ini
+        $saldo_selama_ini = $uang_masuk_selama_ini->nominal - $uang_keluar_selama_ini->nominal;
+
+        return view('account.dashboard.index', compact('saldo_selama_ini','saldo_bulan_ini', 'saldo_bulan_lalu'));
     }
 
 }
--- resources/views/account/dashboard/index.blade.php
+++ resources/views/account/dashboard/index.blade.php
@@ -8,22 +8,37 @@
     <div class="main-content">
         <section class="section">
             <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
                     <div class="card card-statistic-2">
                         <div class="card-icon shadow-primary bg-primary">
                             <i class="fas fa-money-check-alt"></i>
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>SALDO SEKARANG</h4>
+                                <h4>SEMUA SALDO </h4>
                             </div>
-                            <div class="card-body">
-                                Rp. 187,13.000
+                            <div class="card-body" style="font-size: 20px">
+                                {{ rupiah($saldo_selama_ini) }}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <div class="card card-statistic-2">
+                        <div class="card-icon shadow-primary bg-primary">
+                            <i class="fas fa-money-check-alt"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>SALDO BULAN INI</h4>
+                            </div>
+                            <div class="card-body" style="font-size: 20px">
+                                {{ rupiah($saldo_bulan_ini) }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
                     <div class="card card-statistic-2">
                         <div class="card-icon shadow-primary bg-primary">
                             <i class="fas fa-money-check-alt"></i>
@@ -32,8 +47,8 @@
                             <div class="card-header">
                                 <h4>SALDO BULAN LALU</h4>
                             </div>
-                            <div class="card-body">
-                                Rp. 4,732.000
+                            <div class="card-body" style="font-size: 20px">
+                                {{ rupiah($saldo_bulan_lalu) }}
                             </div>
                         </div>
                     </div>
